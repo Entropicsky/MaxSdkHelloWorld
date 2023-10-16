825a9c2c5a0df04ec7b5fa9e1c61ba759e01d7c2
Lawrence Parker <larry@answerrocket.com>
2023-10-16 12:00:13 -0400

    Max SDK hello world!

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+from answer_rocket import AnswerRocketClient
+import json
+
+def call_me():
+    client = AnswerRocketClient()
+    j = client.config.get_artifact('test.json')
+    return json.loads(j)
+
+def get_greeting():
+    client = AnswerRocketClient()
+    greeting = client.config.get_artifact('greeting.html')
+    return greeting
+
+if __name__ == '__main__':
+    x = get_greeting()
+    print(x)
